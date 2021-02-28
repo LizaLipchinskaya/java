7bf9452cc0e9d39fcb8a6d8e76b701152d05f075
LizaLipchinskaya <79473180+LizaLipchinskaya@users.noreply.github.com>
2021-02-28 14:10:46 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,20 @@
 package com.company;
 
+import java.io.Reader;
 import java.lang.*;
 
-import static com.company.Сonstants.InputFileNameIndex;
-import static com.company.Сonstants.OutputFileNameIndex;
+import static com.company.Сonstants.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            MakeCSV makeCSV = new MakeCSV();
-            makeCSV.fileReader(args[InputFileNameIndex]);
-            makeCSV.parsing();
-            makeCSV.writeFile(args[OutputFileNameIndex]);
+            WordsReader wordsReader = new WordsReader();
+            Reader reader = wordsReader.read(args[InputFileNameIndex]);
+            WordsCountCalculator wordsCountCalculator = new WordsCountCalculator();
+            wordsCountCalculator.calculate(reader);
+            CsvWriter csvWriter = new CsvWriter(wordsCountCalculator.getCount(), wordsCountCalculator.getList());
+            csvWriter.write(args[OutputFileNameIndex]);
         } catch (Exception e) {
             System.out.println(e);
         }
